75dcccf00aa03ac88bc84e08d665ae7bb79ce531
DenisKaizer <1kaizerd@gmail.com>
2019-11-10 06:52:01 -0500

    config

--- config/config.go
+++ config/config.go
@@ -1,2 +1,20 @@
 package config
 
+type Channel struct {
+	From      int64
+	To        int64
+	ChannelId string
+}
+
+type Ethereum struct {
+	OwnerPrivKey string
+	ContractAddr string
+	RpcUrl       string
+}
+
+var (
+	DiscordAPIToken            string // discord api token
+	DiscordGameNotifyChannelId string // notify channel id
+	DiscordLevelChannel        []Channel
+	Skale                      Ethereum
+)
---
+++ config/configBase.go
@@ -0,0 +1,10 @@
+package config
+
+func init() {
+	Skale.ContractAddr = `0x0a61F498244769a8Ee4be95Fa55e162871921Cb7`
+	Skale.OwnerPrivKey = `00f36093628a4dd0616f438a2b881b345d88fe7206313d232ce56df0c29c3494`
+	Skale.RpcUrl = `http://sip1.skalenodes.com:10046/`
+
+	DiscordAPIToken = `NjQyNjAyMzc0Mjk2NTAyMjcy.XcfBJg.t3qZu8fgUgl9B1C9t5tizZwpXvw`
+	DiscordGameNotifyChannelId = `642602747547484174`
+}
